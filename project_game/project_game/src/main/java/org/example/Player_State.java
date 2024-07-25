6ad5a521ae1c68a4eff6a50bbf8f617ba19d8aad
adamchleba <163834230+adamchleba@users.noreply.github.com>
2024-07-25 22:16:58 +0200

    Delete project_game directory

--- project_game/project_game/src/main/java/org/example/Player_State.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum Player_State {
-    IDLE, IS_JUMPING, IS_WALKING, IS_RUNNING
-
-}
--- project_game/src/main/java/org/example/Items.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public enum Items {
-    SWORD, SHIELD, APPLE, BREAD, GOLDEN_BREAD, ROCK
-
-
-}
